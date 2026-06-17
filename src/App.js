e9119de8333b170d20df3238978f29a4e22c4bee
Gauravector <gauravsavita4455@gmail.com>
2026-06-17 12:14:16 +0530

    App.js error trial1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App" style={{paddingTop: '0px'}}>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/species" element={<Species />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/scenes" element={<Comingsoon pageName="Scenes" />} />
-          <Route path="/map" element={<Comingsoon pageName="Map" />} />
-          <Route path="/social" element={<Comingsoon pageName="Social" />} />
-        </Routes>
-    </BrowserRouter>
+      <Navbar />
       
     </div>
   );
